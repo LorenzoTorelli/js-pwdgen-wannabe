98c81f60b36ef6d74cece41d8b8cdb879904f523
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-04 15:44:08 +0100

    esercizio-completo

---
+++ JavaScript/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Inserisci il nome");
+let cognome = prompt("Inserisci il cognome")
+let colore = prompt("Inserisci il colore preferito")
+
+document.getElementById("result").innerHTML = nome + cognome + colore + 21; 
